d4748a2f64c739cbcee16e3ee0e69d4bd5196ebf
Ihor Kulikovskyi <ihor.kulikovskyi@massive.co>
2021-03-09 14:59:41 +0200

    Updated headless browser start

--- src/stores-checker.js
+++ src/stores-checker.js
@@ -3,7 +3,10 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
 
 const hasPageContent = async (pageUrl, contentSelector) => {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ 
+        headless: true,
+        args: ['--no-sandbox','--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
     await page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 1 });
     await page.goto(pageUrl);
